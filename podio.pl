c60783a25c47a6da98bd10fdb891e3243eb85fec
prs513rosewood <lucas.frerot@gmail.com>
2012-10-03 23:16:41 -0400

    Initial version of the program

--- podio.pl
+++ podio.pl
@@ -1,5 +1,29 @@
 use strict;
 use warnings;
 
-print "Hello World!\n";
+my @songs;
+my($folder, $file, $random, $loop);
 
+while (<>) {
+	if (/^!/) {
+		if (/rand(om)?/) {
+			$random = 1;
+		}
+		if (/loop/) {
+			$loop = 1;
+		}
+	}
+
+	elsif (/^:(.*?)\/?\s*$/) {
+		$folder = $1;
+	}
+
+	elsif  (/^>(.*?)\s*$/) {
+		$file = $1;
+		push(@songs, $folder."/".$file);
+	}
+}
+
+for (my $i = 0 ; $i < scalar(@songs) ; $i++) {
+	system("afplay", $songs[$i]);
+}
